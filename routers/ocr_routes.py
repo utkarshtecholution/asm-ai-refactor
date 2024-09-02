7c72a3ae7c062598f34d674803850fff1f52becd
utkarshtecholution <utkarsh.pateriya@gmail.com>
2024-09-03 01:24:58 +0530

    reverted changes

--- routers/ocr_routes.py
+++ routers/ocr_routes.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from services.camera_stream_configurator import CameraStreamConfigurator
 from settings.swagger_doc import (
     OCR_TAGS
 )
@@ -13,12 +12,5 @@ router = APIRouter(
     path="/ocr_inference"
 )
 async def ocr_inference():
-    camera_stream_object = CameraStreamConfigurator(0)
-    camera_stream_object.stream_flag = False
-    frame = camera_stream_object.org_frame
-    if frame is None:
-        return JSONResponse(content = {"status": "Failed"},  status_code =400)
-    # threading.Thread(target = process_image_ocr, args = (frame, )).start()
-    
     return JSONResponse(content={"status": "request submitted"}, status_code=200)
     
